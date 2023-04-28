521c94215aac03bfbf46eeb06ca6c85b45d55716
archiesmellsXOXOXO <CKetmanee@student.strode-college.ac.uk>
2023-04-28 11:12:16 +0100

    added more code

--- src/App.svelte
+++ src/App.svelte
@@ -2,4 +2,6 @@
   <h1>Hello, im goin to talk about why archie smell?</h1>
   <u><h2>number one</h2></u>
   <p>one reason he smells is that he kisses hot men like me, another reason he smells is because he doesnt brush his teeth and thats not goood! the worst part is that he watches kids pick their nose and eat it... only archie will know, we know xd</p>
+<h2>number 2</h2>
+<p>he sucks poo poo</p>
 </main>
