5b907d98010e4717bab927599347cbf040f4c2b8
archiesmellsXOXOXO <CKetmanee@student.strode-college.ac.uk>
2023-04-28 15:07:35 +0100

    heading

--- src/App.svelte
+++ src/App.svelte
@@ -3,5 +3,9 @@
     <h1><u> Calvin k / Portfolio </u></h1>
     <link rel="text/css" href="style.ccs">
 </head>    
-
+<body>
+    <header>
+        <a href="#" class="logo">logo</a>
+    </header>
+</body>
 </main>
