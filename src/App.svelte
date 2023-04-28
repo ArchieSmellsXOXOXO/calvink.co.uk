a050e64adc771dd0e0fab7ce1ff14e8d32d73e5e
archiesmellsXOXOXO <CKetmanee@student.strode-college.ac.uk>
2023-04-28 12:00:15 +0100

    title

--- src/App.svelte
+++ src/App.svelte
@@ -3,10 +3,6 @@
     <meta charset="utf-8">
     <meta name="viewport" content="width=device-width">
     <title>Your Name</title>
-    <link href="style.css" rel="stylesheet" type="text/css" />
-    <link rel="preconnect" href="https://fonts.gstatic.com">
-<link href="https://fonts.googleapis.com/css2?family=DotGothic16&display=swap" rel="stylesheet">
-  </head>
 
 
 </main>
