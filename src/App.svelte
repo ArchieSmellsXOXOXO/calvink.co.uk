078a04aeb16dcfbcdfa91f06c46a194bf81549b5
ArchieSmellsXOXOXO <CKetmanee@student.strode-college.ac.uk>
2023-06-24 11:26:49 +0100

    ALL

--- src/App.svelte
+++ src/App.svelte
@@ -1,5 +1,16 @@
 <main>
 
-<h1> " ||カルバン|| "  </h1>
+
+<div>
+<nav>
+    <a href="-">home</a>
+    <a href="-">about</a>
+    <a href="-">blog</a>
+    <a href="-">portfolio</a>
+    <a href="-">contact</a>
+    <div class="animated start-home"></div>
+</nav>
+</div>
+
 
 </main>
